469fd0a7ff0ae2bd0ba33563f9bf458e28cec899
Shunliz <zsl6658@126.com>
2017-10-14 12:32:51 +0800

    add heapsort and topk

--- algrithom/src/main/java/algrithom/SortTest.java
+++ algrithom/src/main/java/algrithom/SortTest.java
@@ -12,7 +12,6 @@ public class SortTest {
 				}
 			}
 			this.printlist(a);
-			System.out.println();
 		}
 	}
 	
@@ -31,7 +30,6 @@ public class SortTest {
                 }
             }
             this.printlist(a);
-			System.out.println();
         }
     }
     
@@ -66,14 +64,72 @@ public class SortTest {
     			}
     		}
     		this.printlist(a);
-    		System.out.println();
     	}
     }
 	
+    
+    public void heapSort(int[] a) {
+        int i;
+        for (i = a.length / 2 - 1; i >= 0; i--) {// 构建一个大顶堆
+            this.adjustHeap(a, i, a.length);
+            this.printlist(a);
+        }
+        System.out.println("finish build heap");
+        for (i = a.length-1; i >= 0; i--) {// 将堆顶记录和当前未经排序子序列的最后一个记录交换
+            int temp = a[0];
+            a[0] = a[i];
+            a[i] = temp;
+            this.adjustHeap(a, 0, i);// 将a中前i-1个记录重新调整为大顶堆
+            this.printlist(a);
+        }
+    }
+    
+    /**
+     * 构建大顶堆
+     */
+    public void adjustHeap(int[] a, int i, int len) {
+        int temp, j;
+        temp = a[i];
+        for (j = 2 * i+1; j < len; j *= 2) {// 沿关键字较大的孩子结点向下筛选
+            if (j+1 < len && a[j] > a[j + 1])
+                ++j; // j为关键字中较大记录的下标
+            if (temp <= a[j])
+                break;
+            a[i] = a[j];
+            i = j;
+        }
+        a[i] = temp;
+    }
+    
+    public void topK(int[] a, int k){
+    	int[] b = new int[k];
+    	for (int i=0;i<k;i++){
+    		b[i]=a[i];
+    	}
+    	this.heapSort(b);
+    	System.out.println("build heap finish");
+    	for(int i=k;i<a.length;i++){
+    		if (a[k]>b[k-1]){
+    			b[k-1] = a[k];
+                this.adjustHeap(b, 0, k);
+    		}
+    		this.printlist(b, k);
+    	}
+    	
+    }
+
 	public void printlist(int[] a){
 		for(int i=0;i<a.length;i++){
 			System.out.print(a[i]+" ");
 		}
+		System.out.println();
+	}
+	
+	public void printlist(int[] a, int length){
+		for(int i=0;i<length;i++){
+			System.out.print(a[i]+" ");
+		}
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
@@ -86,7 +142,7 @@ public class SortTest {
 		test.BubbleSort(a2);
 		
 		System.out.println("##########################");
-		int[] a3 = {8,3,2,5,9,3,6};
+		int[] a3 = {8,3,2,5,9,3,6,28,23,22,25,19,33,16};
 		test.QuickSort(a3, 0, a3.length-1);
 		test.printlist(a3);
 		System.out.println();
@@ -95,6 +151,15 @@ public class SortTest {
 		int[] a4 = {8,3,2,5,9,3,6};
 		test.selectSort(a4);
 		test.printlist(a4);
+		
+		System.out.println("##########################");
+		int[] a5 = {8,3,2,5,9,3,6,28,23,22,25,19,33,16};
+		test.printlist(a5);
+		test.heapSort(a5);
+		test.printlist(a5);
+		System.out.println("##########################");
+		test.topK(a5, 7);
+		test.printlist(a5, 7);
 		
 	}
 
